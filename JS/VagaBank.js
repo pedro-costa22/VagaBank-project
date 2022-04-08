df44987973c790dcdc4600678c3d074b1cbc717d
Pedro Henrique <phc.contato22@gmail.com>
2022-04-08 00:02:51 -0300

    Update-project

--- JS/VagaBank.js
+++ JS/VagaBank.js
@@ -6,6 +6,7 @@ const TelaInicial = document.querySelector("#telaDeEntrada");
 const TelaDeLogin = document.querySelector("#TelaDeLogin"); 
 const TelaDeRegitro = document.querySelector('#CriarUmaConta');
 const home = document.querySelector("#home");
+const saldo = document.querySelector("#saldo");
 
 
 setInterval(() => {
@@ -41,13 +42,83 @@ const botaoLogar = document.querySelector("#LoginNoSite").addEventListener('clic
     btn.preventDefault()
     TelaDeLogin.style.display = "none";
     home.style.display = "grid";
+
+    /* ------         ------*/
     animate.forEach((element) =>{
-        setInterval(() =>{
-            element.classList.add(animationClass)
-        }, 200)
+        timerAnimation(element)
     }) 
-})
+});
+
+
+const btnSaldo = document.querySelector("#btn-saldo").addEventListener("click", () =>{
+    home.style.display = "none";
+    saldo.style.display = "grid";
+
+    animateSaldo.forEach((element) =>{
+        timerAnimation(element)
+    })
+
+});
+
 
 
 const animate = document.querySelectorAll('[data-home]');
+const animateSaldo = document.querySelectorAll('[data-saldo');
 const animationClass = 'animate';
+
+function timerAnimation(element){
+    setInterval(() =>{
+        element.classList.add(animationClass)
+    }, 200)
+}
+
+
+const valoresSaldo = document.querySelectorAll(".value");
+const openValue = document.querySelectorAll(".open-value");
+const closeValue = document.querySelectorAll(".btn");
+
+
+
+openValue.forEach((open) =>{
+    open.addEventListener("mouseover", () =>{
+        switch(open){
+            case openValue[0]:
+                valoresSaldo[0].type = "text";
+                break;
+
+            case openValue[1]:
+                valoresSaldo[1].type = "text";
+                break;
+
+            case openValue[2]:
+                valoresSaldo[2].type = "text";
+                break;
+
+            case openValue[3]:
+                valoresSaldo[3].type = "text";
+                break;
+        }
+    })
+
+    open.addEventListener("mouseout", () =>{
+        switch(open){
+            case openValue[0]:
+                valoresSaldo[0].type = "password";
+                break;
+
+            case openValue[1]:
+                valoresSaldo[1].type = "password";
+                break;
+
+            case openValue[2]:
+                valoresSaldo[2].type = "password";
+                break;
+
+            case openValue[3]:
+                valoresSaldo[3].type = "password";
+                break;
+        }
+    })
+})
+
+
---
+++ teste.js
@@ -0,0 +1,7 @@
+const valoresSaldo = document.querySelectorAll(".value");
+const openValue = document.querySelectorAll(".openValue");
+const closeValue = document.querySelectorAll(".closeValue");
+
+openValue.addEventListener("click", () =>{
+    alert("teste")
+})
